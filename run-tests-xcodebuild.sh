6dd312e3bbbf4f2e56a04d06fc5e708d5f7518b5
Paul Evans <evansp2@gmail.com>
2015-11-09 01:17:47 -0500

    updates

--- run-tests-xcodebuild.sh
+++ run-tests-xcodebuild.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-readonly PROJECT_NAME="PEFuelPurchase-Model"
+readonly PROJECT_NAME="Gas Jot Model"
 readonly SDK_VERSION="9.1"
 readonly ARCH="i386"
 export LC_CTYPE=en_US.UTF-8
 
 xcodebuild \
-    -workspace ${PROJECT_NAME}.xcworkspace \
-    -scheme ${PROJECT_NAME}Tests \
+    -workspace "${PROJECT_NAME}.xcworkspace" \
+    -scheme "PEFuelPurchase-ModelTests" \
     -configuration Debug \
     -sdk iphonesimulator${SDK_VERSION} \
     -arch ${ARCH} test
