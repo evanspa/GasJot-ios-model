db120db198409cbe4134c2ffd33f01c38310dd8f
Paul Evans <evansp2@gmail.com>
2015-12-22 12:54:46 -0500

    fixing test run script

--- run-tests-xcodebuild.sh
+++ run-tests-xcodebuild.sh
@@ -2,13 +2,14 @@
 
 readonly PROJECT_NAME="Gas Jot Model"
 readonly SDK_VERSION="9.2"
+readonly DEVICE_NAME="iPhone 5s"
 readonly ARCH="i386"
 export LC_CTYPE=en_US.UTF-8
 
 xcodebuild \
+test \
 -workspace "${PROJECT_NAME}.xcworkspace" \
--scheme "Gas Jot ModelTests" \
+-scheme "${PROJECT_NAME}Tests" \
 -configuration Debug \
 -sdk iphonesimulator${SDK_VERSION} \
--verbose \
--arch ${ARCH} test
+-destination "platform=iOS Simulator,OS=${SDK_VERSION},name=${DEVICE_NAME}"
